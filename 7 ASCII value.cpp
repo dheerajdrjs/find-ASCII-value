5f7ac28e5faff3c0ef1818e4104f7b6399cc73f0
Dheeraj Kumar Sharma <82236967+dheerajdrjs@users.noreply.github.com>
2021-06-08 20:35:17 +0530

    Add files via upload

---
+++ 7 ASCII value.cpp
@@ -0,0 +1,13 @@
+// WAP to print ASCII value of character
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char c;
+	cout<<"Enter a character : ";
+	cin>>c;
+	cout<<endl<<"ASCII value of "<<c<<" is "<<int(c);
+	return 0;
+}
